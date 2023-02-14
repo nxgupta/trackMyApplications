ec6ebea60b0b3fb861fcbec5fb1162bdb568758d
nxgupta <neeraj.gupta6525@gmail.com>
2023-02-14 18:05:28 +0530

    ready for deployment

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -22,10 +22,7 @@ const Landing = () => {
               job <span>tracking</span> app
             </h1>
             <p>
-              I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-              bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-              quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-              narwhal.
+            No more messy job search spreadsheets. Jobify keeps track of every detail about your job opportunities regardless of where you found them
             </p>
             <Link to='/register' className='btn btn-hero'>
               Login/Register
